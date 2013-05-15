da620f6c27667ac6cf6cd1fb695fa3ac9b6a28e3
Matt Amy <meamy@The-Sprawl.(none)>
2013-05-15 14:49:05 -0400

    prepping for publishing online

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+/*--------------------------------------------------------------------
+Tpar - T-gate optimization for quantum circuits
+Copyright (C) 2013  Matthew Amy and The University of Waterloo,
+Institute for Quantum Computing, Quantum Circuits Group
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Matthew Amy
+---------------------------------------------------------------------*/
+
 #include "circuit.h"
 #include <cstdio>
 #include <iomanip>
@@ -18,14 +39,16 @@ int main(int argc, char *argv[]) {
     //	clock_gettime(CLOCK_MONOTONIC, &start);
     synth = c.synthesize();
     //	clock_gettime(CLOCK_MONOTONIC, &end);
-    //	cout << fixed << setprecision(3);
-    //	cout << "Time: " << (end.tv_sec + (double)end.tv_nsec/1000000000) - (start.tv_sec + (double)start.tv_nsec/1000000000) << " s\n";
   } else {
     metacircuit meta;
     meta.partition_dotqc(circuit);
+    //	clock_gettime(CLOCK_MONOTONIC, &start);
     meta.optimize();
+    //	clock_gettime(CLOCK_MONOTONIC, &end);
     synth = meta.to_dotqc();
   }
+  //	cout << fixed << setprecision(3);
+  //	cout << "# Time: " << (end.tv_sec + (double)end.tv_nsec/1000000000) - (start.tv_sec + (double)start.tv_nsec/1000000000) << " s\n";
 
   synth.remove_swaps();
   synth.remove_ids();
--- src/partition.cpp
+++ src/partition.cpp
@@ -1,3 +1,24 @@
+/*--------------------------------------------------------------------
+Tpar - T-gate optimization for quantum circuits
+Copyright (C) 2013  Matthew Amy and The University of Waterloo,
+Institute for Quantum Computing, Quantum Circuits Group
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Matthew Amy
+---------------------------------------------------------------------*/
+
 #include "partition.h"
 
 template<typename T>
